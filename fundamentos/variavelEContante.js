99cac17b18abeea752f4e80f35b4bd5eba233108
nicolas <nicolas.arthur2786@gmail.com>
2021-11-24 20:21:26 -0300

    variaveis e comentarios

---
+++ fundamentos/variavelEContante.js
@@ -0,0 +1,20 @@
+//nunca usar var
+var a = 3
+let b = 4
+
+//melhor criar variaveis com let
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+//const nao pode mudar o valor
+const c = 5
+
+console.log(c)
+
