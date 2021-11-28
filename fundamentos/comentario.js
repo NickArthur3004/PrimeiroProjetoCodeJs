e6631559b6ceb9567d35d514dd8eb15f09c65912
nicolas <nicolas.arthur2786@gmail.com>
2021-11-28 20:13:40 -0300

    strings e math

---
+++ fundamentos/comentario.js
@@ -0,0 +1,17 @@
+//Comentario de uma linha
+console.log("Linha 1");
+
+/*
+Comentario de 
+multiplas linhas
+*/
+
+console.log("Linha 2");
+
+/*
+ *Comentario 
+ *multiplas 
+ *linhas
+ */
+
+ console.log("Linha 3")
