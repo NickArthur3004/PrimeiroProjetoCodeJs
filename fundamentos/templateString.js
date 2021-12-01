61db4c63f9fe221e81179e6af0fbccc732a2c22a
nicolas <nicolas.arthur2786@gmail.com>
2021-12-01 13:24:59 -0300

    string e booleans

---
+++ fundamentos/templateString.js
@@ -0,0 +1,12 @@
+const nome = "Rebeca"
+const  concatenacao = 'ola ' + nome + "!"
+const template = `
+        Ola
+        ${nome}!`
+
+console.log(concatenacao, template)
+
+//expressões...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
